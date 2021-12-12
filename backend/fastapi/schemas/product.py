b9755640b43becfc2bd1eb083ea84df8ead832a9
vagrant <vagrant@ubuntu2004.localdomain>
2021-12-12 12:18:53 +0000

    gunicorn launch setup in docker

--- backend/fastapi/schemas/product.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-
-class Product(BaseModel):
-    id: int = Field(None)
-    rounded_square_icon: bytes
-    circle_icon: bytes
-
-    class Config:
-        orm_mode = True
--- backend/mysql/initdb.d/testdata.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO user (user_id, img, img_name) VALUES ("82f0a5d73a544df19043ea81a270baed", "./img/test1.jpg", "first")
-INSERT INTO user (user_id, img, img_name) VALUES ("37647b9219ac4913a354ff022318e982", "./img/test2.jpg", "second")
-INSERT INTO user (user_id, img, img_name) VALUES ("f1abe2ed0461426ca3e2ba7235344fc0", "./img/test3.jpg", "third")
