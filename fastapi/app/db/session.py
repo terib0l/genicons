32156a2d223ee53b67eda406ae4656c66e856bae
terib0l <64969840+terib0l@users.noreply.github.com>
2022-07-26 02:44:19 +0900

    Update session.py

--- fastapi/app/db/session.py
+++ fastapi/app/db/session.py
@@ -4,9 +4,11 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from app.core.config import DATABASE_URL
+from app.core.config import MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_NAME
 
-SQLALCHEMY_DATABASE_URL = re.sub("mysql", "mysql+aiomysql", DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = f"mysql+aiomysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_NAME}?charset=utf8mb4"
+# SQLALCHEMY_DATABASE_URL = re.sub("mysql", "mysql+aiomysql", DATABASE_URL)
+
 Base = declarative_base()
 
 ENGINE = create_async_engine(
